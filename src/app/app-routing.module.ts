3df1c7d0505e50b21ceaebafa693de6d3b965490
brin <fabianbrin@gmail.com>
2021-01-22 16:10:51 +0100

    (feat: router and homepage)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,59 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SigninComponent } from './signin/signin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: 'homepage',
+    component: HomepageComponent
+  },
+  {
+    path: 'register',
+    component: SigninComponent
+  }, 
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'userprofile',
+    component: ProfileComponent
+  },
+  {
+    path: 'userprofile/:id',
+    component: ProfileComponent
+  },
+  {
+    path: 'serverprofile',
+    component: HomepageComponent
+  },
+  {
+    path: 'serverprofile/:id',
+    component: HomepageComponent
+  },
+  {
+    path: 'adminprofile',
+    component: HomepageComponent
+  },
+  {
+    path: 'adminprofile/:id',
+    component: HomepageComponent
+  },
+   /// Fallbacks
+   { path: '', redirectTo: 'homepage', pathMatch: 'full' },
+   { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    { enableTracing: true } // <-- debugging purposes only
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { SigninComponent } from './signin/signin.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    SigninComponent,
+    LoginComponent,
+    ProfileComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
